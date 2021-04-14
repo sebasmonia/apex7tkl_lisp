0477effca0af6c9e86e23b9effa43d92d755d25e
Sebastián Monía <sebastian.monia@starz.com>
2021-04-14 14:39:02 -0600

    Open the device using usb-simple-setup

--- apex7tkl.lisp
+++ apex7tkl.lisp
@@ -4,6 +4,9 @@
 
 (defun get-apex7tkl-device ()
   "Return the ."
-  (let ((vendor-id 4152)
-        (product-id 5656))
-    (car (cl-libusb:usb-get-devices-by-ids vendor-id product-id))))
+  (let* ((vendor-id 4152)
+         (product-id 5656)
+         (device (car (cl-libusb:usb-get-devices-by-ids vendor-id product-id))))
+    (when device
+      (cl-libusb:usb-simple-setup device))
+    device))
